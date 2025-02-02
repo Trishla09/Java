25254cec933338c438b5ca766f0968e14d7f7c1e
Trishla09 <trishladeepu1209@gmail.com>
2025-02-02 09:49:36 +0530

    Add files via upload

---
+++ Basic/SizeOfInteger.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class SizeOfInteger{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number:");
+int a=sc.nextInt();
+int s=Integer.SIZE;
+System.out.println("The size of a is "+s);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
